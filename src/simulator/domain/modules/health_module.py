56acbfec0b790a7c747a49e448e1e23d84a42914
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-07-14 11:01:53 +0100

    feat: added death curve to health module

--- src/simulator/domain/modules/health_module.py
+++ src/simulator/domain/modules/health_module.py
@@ -1,12 +1,17 @@
 # ================================================================
 # 0. Section: IMPORTS
 # ================================================================
+from __future__ import annotations
+
 import numpy as np
 
-from typing import ClassVar
+from typing import ClassVar, TYPE_CHECKING
 from dataclasses import dataclass
 
 from .node_module import NodeModule
+
+if TYPE_CHECKING:
+    from ..simulation_state import SimulationState
 
 
 # ================================================================
@@ -18,6 +23,16 @@ class HealthModule(NodeModule):
 
     health: float
     age: float
+    decay_factor: float
+    max_age: float
 
-    def apply(self, rng: np.random.Generator):
+    def apply(self, previous_state: SimulationState, rng: np.random.Generator):
+        self.age += previous_state.time_step.factor
+        self.health = decay_curve(self.age, self.decay_factor, self.max_age)
+
+    def _compute_death_chance(self) -> float:
         pass
+
+def decay_curve(x: float, decay_factor: float, max_age: float) -> float:
+    c = decay_factor
+    return 100 * np.log(1 + c * (1 - x / max_age)) / np.log(1 + c)
