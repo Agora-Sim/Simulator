4767d8b0b5325d6723d024d0e1511512aedcda47
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-07-16 10:22:48 +0100

    feat: metric plot added

---
+++ src/simulator/adapters/render/metric_plot.py
@@ -0,0 +1,31 @@
+# ================================================================
+# 0. Section: IMPORTS
+# ================================================================
+from matplotlib import pyplot as plt
+
+from dataclasses import dataclass
+from matplotlib.figure import Figure
+
+from ...domain.analysis import MetricSeries
+
+
+# ================================================================
+# 1. Section: Functions
+# ================================================================
+@dataclass
+class MetricPlot:
+    series: MetricSeries
+
+    def render(self) -> Figure:
+        plt.figure()
+        plt.plot(self.series.timepoints, self.series.mean)
+        plt.fill_between(
+            self.series.timepoints,
+            self.series.mean - self.series.std,
+            self.series.mean + self.series.std,
+            alpha=0.2,
+        )
+        plt.xlabel(f"Time ({self.series.time_unit})")
+        plt.ylabel(f"{self.series.name} ({self.series.unit})")
+        plt.title(self.series.name)
+        return plt.gcf()
